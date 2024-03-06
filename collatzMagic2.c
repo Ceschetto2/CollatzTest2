e022038219c1ca2350bf805d9d6fa5d274df3677
Francesco <francesco.bocci09@gmail.com>
2024-03-06 21:02:01 +0100

    Risolto problema logico xD

--- collatzMagic2.c
+++ collatzMagic2.c
@@ -12,8 +12,8 @@ int main()
 
     while (1)
     {
-        gmp_printf("testing: %Zd\n", n_org);
         mpz_add_ui(n_org, n_org, 1);
+        gmp_printf("testing: %Zd\n", n_org);
         collatz(n_org);
     }
     
@@ -26,24 +26,27 @@ int main()
 int collatz(mpz_t n_org)
 {
     mpz_t n;
+
     mpz_init(n);
     mpz_set(n, n_org);
     do
     {
 
-        if (mpz_odd_p(n))
+        if (!mpz_odd_p(n))
         {
             mpz_div_ui(n, n, 2);
         }
         else
         {
-            mpz_addmul_ui(n, n, 3);
+            mpz_addmul_ui(n, n, 2);
             mpz_add_ui(n, n, 1);
         }
         
     }while(collatz_condition(n, n_org));
 
+
     mpz_clear(n);
+            
 }
 
 int collatz_condition(mpz_t n, mpz_t n_org)
